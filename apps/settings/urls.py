b724203c6c5114ae2020a4f4e1b9e3a7d27defa6
nurai003002 <nuraj9663@gmail.com>
2023-12-10 22:04:09 +0600

    statics

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.settings.views import index
+from apps.settings.views import index1, index2, index3,index4
 
 urlpatterns = [
-    path('', index,name='index')
-
+    path('', index1, name='index1'),
+    path('1', index2, name='index2'),
+    path('2', index3, name='index3'),
+    path('3', index4, name='index4')
 ]
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'home2.html', locals())
+def index1(request):
+    return render(request, 'blog-post.html', locals())
 
+def index2(request):
+    return render(request, 'blog.html', locals())
+
+def index3(request):
+    return render(request, 'index-kenburns.html', locals())
+
+def index4(request):
+    return render(request, 'index-slideshow.html', locals())
